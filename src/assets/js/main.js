0b6d3b360d727f57ef3e90aa7721151228fafbdb
Henrique <69041032+ApiLogik@users.noreply.github.com>
2021-06-09 12:52:09 -0300

    Add files via upload

---
+++ src/assets/js/main.js
@@ -0,0 +1,37 @@
+import { carouselBuilder } from '../../modules/hcarrousel.js'
+import '../css/style.css'
+
+const onLoad = () => {
+	//Reload if width has changed
+	const initialWidth = window.innerWidth
+
+	const checkWidthChange = () => {
+		let actualWidth = window.innerWidth
+		actualWidth !== initialWidth ? setTimeout(() => location.reload(),0) : false
+	}
+
+	window.addEventListener('resize', checkWidthChange)
+
+	//Select proper carousel - desktop or mobile
+	const loadCarousel = () => {
+		return window.innerWidth >= 420 ? document.querySelector('.carousel-desktop') : document.querySelector('.carousel-mobile')
+	}
+
+	const choosedCarousel = loadCarousel()
+
+	//create carousel object
+	const carousel = {
+		slidesContainer: choosedCarousel, //container - all slides
+		allSlides: choosedCarousel.querySelectorAll('.slide-single'), //DOM object w/ each single slide
+		bulletsContainer: document.querySelector('.carousel-bullets'), //navigation bullets container
+		arrowLeft: document.querySelector('#navprev'),
+		arrowRight: document.querySelector('#navnext'),
+		speed: 500, //speed of animation (ms)
+		interval: 5000 //interval between slides (ms)
+	}
+	
+	//On load functions
+	carouselBuilder(carousel);
+}
+
+onLoad();
